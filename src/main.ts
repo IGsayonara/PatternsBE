1cbf410fa2e83be59cf149dcbdbd0708c46fc9a5
Igor <diduniki@gmail.com>
2023-11-06 17:47:43 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Patterns API')
+    .setDescription('This api provides you some abilities')
+    .setVersion('1.0')
+    .addTag('TransitionCalculator')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 
